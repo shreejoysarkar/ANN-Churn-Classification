0c17a98d320cb01a5e1cc27f57c9087bf3b3a949
shreejoy <sarkarshreejoy30@gmail.com>
2026-01-09 23:11:44 +0530

    streamlit implementation

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+import pandas as pd
+import pickle
+
+## Load the trained model
+
+model = tf.keras.models.load_model("model.h5")
+
+## Load the encoders and scaler
+
+with open('ENCODERS/label_encoder_gender.pkl','rb') as f:
+    label_encoder_gender = pickle.load(f)
+
+with open('ENCODERS/onehot_encoder_geo.pkl','rb') as f:
+    onehot_encoder_geo = pickle.load(f)
+
+with open('scaler.pkl','rb') as f:
+    scaler = pickle.load(f)
+
+
+
+## Streamlit APP
+
+st.title('Customer Churn Prediction')
+
+## Input fields
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider("Age", 18,92)
+balance = st.number_input("Balance")
+credit_score = st.number_input("Credit Score")
+estimated_salary = st.number_input("Estimated Salary")
+tenure = st.slider('Tenure', 0,10)
+num_of_products  = st.slider('Number of Products', 1,4)
+has_cr_card = st.selectbox("has Credit Card", [0,1])
+is_active_member = st.selectbox("is Active Member", [0,1])
+
+
+## Prepare the input data
+
+input_Data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age':[age],
+    'Tenure':[tenure],
+    'Balance':[balance],
+    'NumOfProducts':[num_of_products],
+    'HasCrCard':[has_cr_card],
+    'IsActiveMember':[is_active_member],
+    'EstimatedSalary':[estimated_salary]
+})
+
+## One-Hot Encode the 'Geography' column
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns= onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+
+## concat the encoded geography columns with the input dataframe
+input_Data = pd.concat([input_Data.reset_index(drop=True),geo_encoded_df], axis=1)
+
+## Scale the input data
+input_data_scaled = scaler.transform(input_Data)
+
+## Predict button
+prediction= model.predict(input_data_scaled)
+prediction_proba = prediction[0][0]
+
+if prediction_proba > 0.5:
+    st.write(f'The customer is likely to churn with a probability of {prediction_proba:.2f}')
+
+else:
+    st.write(f'The customer is not likely to churn with a probability of {1 - prediction_proba:.2f}')
